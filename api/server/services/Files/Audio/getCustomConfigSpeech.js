28d6c487ab9b75e2e23cdbab172a25c83fa2941b
Megan Greenberg <mgreenberg@networkninja.com>
2025-07-03 15:30:10 -0400

    feat: added config for SST and TTS

--- api/server/services/Files/Audio/getCustomConfigSpeech.js
+++ api/server/services/Files/Audio/getCustomConfigSpeech.js
@@ -25,11 +25,13 @@ async function getCustomConfigSpeech(req, res) {
       });
     }
 
-    const sttExternal = !!appConfig.speech?.stt;
-    const ttsExternal = !!appConfig.speech?.tts;
+    const sttExternal = !!customConfig.speech?.stt;
+    const ttsExternal = !!customConfig.speech?.tts;
+    const browserDisabled = !!customConfig.speech?.speechTab.browserDisabled;
     let settings = {
       sttExternal,
       ttsExternal,
+      browserDisabled,
     };
 
     if (!appConfig.speech?.speechTab) {
@@ -40,6 +42,10 @@ async function getCustomConfigSpeech(req, res) {
 
     if (speechTab.advancedMode !== undefined) {
       settings.advancedMode = speechTab.advancedMode;
+    }
+
+    if (speechTab.browserDisabled !== undefined) {
+      settings.browserDisabled = speechTab.browserDisabled;
     }
 
     if (speechTab.speechToText) {
