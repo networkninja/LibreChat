a3db65193a58cad76a79b7470f069051c4029281
Megan Greenberg <mgreenberg@networkninja.com>
2025-07-15 14:15:02 -0400

    accidently merged my code, removing code. sorry

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -417,7 +417,6 @@ const speechTab = z
   .object({
     conversationMode: z.boolean().optional(),
     advancedMode: z.boolean().optional(),
-    browserDisabled: z.boolean().optional(),
     speechToText: z
       .boolean()
       .optional()
