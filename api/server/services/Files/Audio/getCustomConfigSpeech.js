3e4731a2e41945ab96c8ce88ea77260509b638d3
Megan Greenberg <mgreenberg@networkninja.com>
2025-07-15 14:15:02 -0400

    accidently merged my code, removing code. sorry

--- api/server/services/Files/Audio/getCustomConfigSpeech.js
+++ api/server/services/Files/Audio/getCustomConfigSpeech.js
@@ -27,11 +27,9 @@ async function getCustomConfigSpeech(req, res) {
 
     const sttExternal = !!customConfig.speech?.stt;
     const ttsExternal = !!customConfig.speech?.tts;
-    const browserDisabled = !!customConfig.speech?.speechTab.browserDisabled;
     let settings = {
       sttExternal,
       ttsExternal,
-      browserDisabled,
     };
 
     if (!appConfig.speech?.speechTab) {
