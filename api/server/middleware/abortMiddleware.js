3f0f7d93246fdd2187ebee3a9ab7c2ec965af836
Megan Greenberg <mgreenberg@networkninja.com>
2025-07-28 12:22:53 -0400

    feat:removing duplicates

--- api/server/middleware/abortMiddleware.js
+++ api/server/middleware/abortMiddleware.js
@@ -2,7 +2,6 @@ const { logger } = require('@librechat/data-schemas');
 const { countTokens, isEnabled, sendEvent } = require('@librechat/api');
 const { isAssistantsEndpoint, ErrorTypes } = require('librechat-data-provider');
 const { getCustomConfig } = require('~/server/services/Config');
-const { sendMessage, sendError, countTokens, isEnabled } = require('~/server/utils');
 const { truncateText, smartTruncateText } = require('~/app/clients/prompts');
 const clearPendingReq = require('~/cache/clearPendingReq');
 const { sendError } = require('~/server/middleware/error');
